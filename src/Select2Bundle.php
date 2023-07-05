3a90d9dd06350520314f9814bf3ab6d18a025bd6
Francisco Suárez <fsuarezm@gmail.com>
2023-07-05 12:52:20 +0200

    Commit inicial

---
+++ src/Select2Bundle.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of fsuarezm/ux-select2
+ *
+ * (c) Francisco Suárez Mulero
+ * @author: Francisco Suárez Mulero
+ * @email: fsuarezm@gmail.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace FSM\Symfony\UX\Select2;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @final
+ */
+class Select2Bundle extends Bundle
+{
+}
